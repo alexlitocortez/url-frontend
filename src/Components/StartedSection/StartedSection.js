82043e0e67e68ef00a990b5d3a0b1b15f8849591
Alex Cortez <acim650@gmail.com>
2021-10-09 10:23:48 -0700

    src/Components/StartedSection/

---
+++ src/Components/StartedSection/StartedSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './StartedSection.css';
+
+
+function StartedSection() {
+    return (
+        <div>
+            <div className='startedSection'>
+                <div className='moneyMaker'>
+                    Boost your links today
+                </div>
+                <button className='startedSectionButton'>Get Started</button>
+            </div>
+        </div>
+    )
+}
+
+export default StartedSection
