126603f2b073011853865e8a0771843f90aa13a9
Alex Cortez <acim650@gmail.com>
2021-10-26 09:21:51 -0700

    src/Components/Hamburger/

---
+++ src/Components/data/NavData.js
@@ -0,0 +1,11 @@
+
+export const NavData = [
+    { title: 'Features', link: '/' },
+    { title: 'Pricing', link: '/' },
+    { title: 'Resources', link: '/' },
+    { title: 'Login', link: '/' },
+    { title: 'Sign Up', link: '/' }
+]
+
+
+
