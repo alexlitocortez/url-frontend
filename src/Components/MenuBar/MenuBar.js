849ad476e09438c2da7ef362380f0bd7b4ba74be
Alex Cortez <acim650@gmail.com>
2021-11-29 08:51:13 -0800

    src/Components

--- src/Components/MenuBar/MenuBar.js
+++ src/Components/MenuBar/MenuBar.js
@@ -4,13 +4,16 @@ import { FaTimes } from 'react-icons/fa';
 import { NavData } from '/Users/l/url-frontend/src/Components/data/NavData.js';
 import { Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import './MenuBar.css';
+
 
 const DropdownContainer = styled.div`
     position: fixed;
     z-index: 999;
-    width: 100%;
-    height: 100%;
-    background: #696969;
+    width: 95vw;
+    padding: 1.5rem 2rem;
+    border-radius: 0.5rem;
+    background-color: #3b3054;
     display: grid;
     align-items: center;
     top: 0;
@@ -38,22 +41,18 @@ const CloseIcon = styled(FaTimes)`
 const DropdownDashboard = ({open, toggle}) => {
 
     return (
-        <DropdownContainer>
-            <Icon onClick={toggle} >
-                <CloseIcon onClick={toggle} />
-            </Icon>
-
-            <Grid>
-
-                <Grid item xs={6}>
-                {NavData.map((item, index) => (
-                    <Typography style={{ padding: '20px' }} tp={item.link} key={index}>
-                        {item.title}
-                    </Typography>
-                ))}
-                </Grid>
-            </Grid>
-        </DropdownContainer>
+        <div>
+            <DropdownContainer>
+                <ul className='nav-list'>
+                    <li className='nav-list-item'>Features</li>
+                    <li className='nav-list-item'>Pricing</li>
+                    <li className='nav-list-item'>Resources</li>
+                </ul>
+                <hr></hr>
+                <button className='login-button'>Login</button>
+                <button className='signup-button'>Sign Up</button>
+            </DropdownContainer>
+        </div>
     )
 }
 
